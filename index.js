c66dcb60ed53ff0c2148af86aad71513020f5bbf
Mark Gowen <mark.gowen78@gmail.com>
2022-11-03 16:17:19 -0700

    commit tree f032b37851a82a576fb1b924406dfb1625cae931

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const names = [];
+const eventName = "";
+
+function writeCards(names, eventName) {
+    const newArr = [];
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    };
+    console.log(newArr);
+    return newArr;
+
+}
+
+writeCards(names, eventName);
+
+function countDown(n) {
+    while (n > 0) {
+       console.log(n);
+       n -= 1;
+    };
+    console.log(n);
+}
+
+
+
+
+
